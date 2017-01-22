442683aa2d2dac6d73520ba4d91e756ba0aa046b
mauro-santos <santos.mauroantonio@gmail.com>
2017-01-22 16:35:13 -0300

    - Alterações das lições com ListView.

--- app/src/main/java/br/com/maurosantos/android/sunshine/MainActivity.java
+++ app/src/main/java/br/com/maurosantos/android/sunshine/MainActivity.java
@@ -1,7 +1,11 @@
 package br.com.maurosantos.android.sunshine;
 
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,36 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.container, new PlaceholderFragment())
+                    .commit();
+        }
+    }
+
+    public static class PlaceholderFragment extends Fragment {
+
+        public PlaceholderFragment() {
+            // Required empty public constructor
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+            // Inflate the layout for this fragment
+            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+
+            String[] forecast = new String[]{
+                    "Today",
+                    "Tomorrow",
+                    "Weds",
+                    "Thurs",
+                    "Fri",
+                    "Sat"
+            };
+
+            return rootView;
+        }
     }
 }
